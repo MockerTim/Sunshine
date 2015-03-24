e4d1ed15364e42ebc998d1612bb0952ba2ad5715
mockertim <t.shakuov@gmail.com>
2015-03-24 09:30:17 +0300

    Minor changes

--- app/src/main/java/com/example/android/sunshine/app/data/WeatherContract.java
+++ app/src/main/java/com/example/android/sunshine/app/data/WeatherContract.java
@@ -41,6 +41,16 @@ public class WeatherContract {
     public static final class LocationEntry implements BaseColumns {
         public static final String TABLE_NAME = "location";
 
+        public static final String COLUMN_LOCATION_SETTING = "location_setting";
+        // Location city name, for instance: "Moscow,ru"
+        public static final String COLUMN_CITY_NAME = "city_name";
+
+        // Location latitude
+        public static final String COLUMN_COORD_LAT = "coord_lat";
+
+        // Location longitude
+        public static final String COLUMN_COORD_LONG = "coord_long";
+
     }
 
     /* Inner class that defines the table contents of the weather table */
