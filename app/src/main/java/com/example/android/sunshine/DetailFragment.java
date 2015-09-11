05935c6b98c9fd10406a15ae4293ba21ab9a7008
t.shakuov <t.shakuov@gmail.com>
2015-09-11 11:21:05 +0300

    Lesson 4C. Complited.

--- app/src/main/java/com/example/android/sunshine/DetailFragment.java
+++ app/src/main/java/com/example/android/sunshine/DetailFragment.java
@@ -1,9 +1,13 @@
 package com.example.android.sunshine;
 
+import android.database.Cursor;
 import android.support.v4.app.Fragment;
 //import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v4.view.MenuItemCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,15 +20,45 @@ import android.view.ViewGroup;
 import android.support.v7.widget.ShareActionProvider;
 import android.widget.TextView;
 
+import com.example.android.sunshine.app.Utility;
+import com.example.android.sunshine.app.data.WeatherContract;
+
 /**
  * Created by timur on 08.01.15.
  */
-public class DetailFragment extends Fragment {
+public class DetailFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final String LOG_TAG = DetailFragment.class.getSimpleName();
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+
+//    private ShareActionProvider mShareActionProvider;
     private String mForecastString;
 
     ShareActionProvider shareActionProvider;
+
+    private static final int DETAIL_LOADER = 0;
+
+    private static final String[] FORECAST_COLUMNS = {
+            // In this case the id needs to be fully qualified with a table name, since
+            // the content provider joins the location & weather tables in the background
+            // (both have an _id column)
+            // On the one hand, that's annoying.  On the other, you can search the weather table
+            // using the location set by the user, which is only in the Location table.
+            // So the convenience is worth it.
+            WeatherContract.WeatherEntry.TABLE_NAME + "." + WeatherContract.WeatherEntry._ID,
+            WeatherContract.WeatherEntry.COLUMN_DATE,
+            WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
+            WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
+            WeatherContract.WeatherEntry.COLUMN_MIN_TEMP
+    };
+
+    // These indices are tied to FORECAST_COLUMNS.  If FORECAST_COLUMNS changes, these
+    // must change.
+    public static final int COL_WEATHER_DATE = 2;
+    public static final int COL_WEATHER_DESC = 3;
+    public static final int COL_WEATHER_MAX_TEMP = 6;
+    public static final int COL_WEATHER_MIN_TEMP = 5;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,8 +77,8 @@ public class DetailFragment extends Fragment {
 
         shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
 
-        if(shareActionProvider != null) {
-            shareActionProvider.setShareIntent(createShareIntent());
+        if(mForecastString != null) {
+            shareActionProvider.setShareIntent(createForecastShareIntent());
         } else {
             if(Constants.D) Log.e(Constants.TAG, "Share Action Provider is null!?");
         }
@@ -72,7 +106,7 @@ public class DetailFragment extends Fragment {
             if(intent != null) {
                 String message = intent.getStringExtra(Intent.EXTRA_TEXT);
 
-                //setShareIntent(createShareIntent());
+                //setShareIntent(createForecastShareIntent());
             } else {
                 if(Constants.D) Log.d(Constants.TAG, "Intent is null!");
             }
@@ -82,9 +116,7 @@ public class DetailFragment extends Fragment {
 
         if(Constants.D) Log.d(Constants.TAG, "<[DetailFragment.onOptionsItemSelected()]");
 
-        //return result;
         return super.onOptionsItemSelected(item);
-//        return true;
     }
 
     @Override
@@ -95,15 +127,17 @@ public class DetailFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
 
         if(intent != null) {
-            mForecastString = intent.getStringExtra(Intent.EXTRA_TEXT);
-            TextView textView = (TextView) rootView.findViewById(R.id.detail_text);
-            textView.setText(mForecastString);
+            mForecastString = intent.getDataString();
+        }
+
+        if(null != mForecastString) {
+            ((TextView) rootView.findViewById(R.id.detail_text)).setText(mForecastString);
         }
 
         return rootView;
     }
 
-    private Intent createShareIntent() {
+    private Intent createForecastShareIntent() {
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
         shareIntent.setType("text/plain");
@@ -117,5 +151,49 @@ public class DetailFragment extends Fragment {
         } else {
             if(Constants.D) Log.e(Constants.TAG, "shareActionProvider is null!");
         }
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        Log.v(LOG_TAG, "In onCreateLoader");
+
+        Intent intent = getActivity().getIntent();
+
+        if(intent == null) {
+            return null;
+        }
+
+        return new CursorLoader(getActivity(), intent.getData(), FORECAST_COLUMNS, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        Log.v(LOG_TAG, "In onLoadFinished");
+
+        if(!data.moveToFirst()) {return;}
+
+        String dateString = Utility.formatDate(data.getLong(COL_WEATHER_DATE));
+
+        String weatherDescription = data.getString(COL_WEATHER_DESC);
+
+        boolean isMetric = Utility.isMetric(getActivity());
+
+        String high = Utility.formatTemperature(data.getDouble(COL_WEATHER_MAX_TEMP), isMetric);
+
+        String low = Utility.formatTemperature(data.getDouble(COL_WEATHER_MIN_TEMP), isMetric);
+
+        mForecastString = String.format("%s - %s - %s/%s", dateString, weatherDescription, high, low);
+
+        TextView detailTextView = (TextView) getView().findViewById(R.id.detail_text);
+        detailTextView.setText(mForecastString);
+
+        if(shareActionProvider != null) {
+            shareActionProvider.setShareIntent(createForecastShareIntent());
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -32,8 +32,13 @@ public class MainActivity extends ActionBarActivity {
 
     private final String LOG_TAG = MainActivity.class.getSimpleName();
 
+    private final String FORECASTFRAGMENT_TAG = "FFTAG";
+
+    private String mLocation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mLocation = Utility.getPreferredLocation(this);
         super.onCreate(savedInstanceState);
 
         Log.d(Constants.TAG, "[MainActivity.onCreate()]");
@@ -47,7 +52,7 @@ public class MainActivity extends ActionBarActivity {
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new ForecastFragment())
+                    .add(R.id.container, new ForecastFragment(), FORECASTFRAGMENT_TAG)
                     .commit();
         }
     }
@@ -96,6 +101,20 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String location = Utility.getPreferredLocation( this );
+        // update the location in our second pane using the fragment manager
+        if (location != null && !location.equals(mLocation)) {
+            ForecastFragment ff = (ForecastFragment)getSupportFragmentManager().findFragmentByTag(FORECASTFRAGMENT_TAG);
+            if ( null != ff ) {
+                ff.onLocationChanged();
+            }
+            mLocation = location;
+        }
     }
 
     /**
